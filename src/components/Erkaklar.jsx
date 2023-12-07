8b4a3ef0a0484842c186ebd8833b5c0fa474b66d
gulomovx <xgulomov447@gmail.com>
2023-12-07 18:14:07 +0500

    landing

--- src/components/Erkaklar.jsx
+++ src/components/Erkaklar.jsx
@@ -18,7 +18,7 @@ const Erkaklar = () => {
         <HonimCart img={img3}/>
         <HonimCart img={img4}/>
         </div>
-        <button className='px-6 py-4 group hover:shadow-lg hover:shadow-red-500 duration-300 bg-red-600 text-white flex items-center gap-2 mt-4'>
+        <button className='px-6 py-4 group hover:shadow-lg hover:shadow-red-500 duration-300 bg-red-600 text-white flex items-center gap-2 mt-6'>
           BATAFSIL
            <GoArrowRight size={20} className='group-hover:translate-x-1 transition'/>
         </button>
--- src/components/HonimCart.jsx
+++ src/components/HonimCart.jsx
@@ -5,15 +5,15 @@ const HonimCart = ({ img }) => {
     
   return (
       <div>
-          <div className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat z-0">
-        <img src={img} alt="" className='transition duration-300 ease-in-out hover:scale-110' />
+          <div className="transition duration-300 hover:scale-105">
+        <img src={img} alt="" className='' />
         <div className="z-10">
 
-              <span className="bg-red-600 text-white rounded-2xl flex items-center justify-center absolute w-16 top-2 left-60">
+              <span className="bg-red-600 text-white rounded-2xl flex items-center justify-center relative w-16 bottom-[410px] left-60">
                   <FaStar color='yellow'/>
                   4.95</span>
               <p className="text-slate-500">AYOLLAR UCHUN</p>
-              <h1 className="text-slate-500">Kastyum Turi</h1>
+              <h1 className="text-slate-900 text-[28px]">Kastyum Turi</h1>
           <p className="text-slate-500">ID XXX</p>
         </div>
           
